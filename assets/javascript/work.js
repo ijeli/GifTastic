1086b7c884b5bce257df637d023e9c07f7c92215
ijeli <legaspi.jel@gmail.com>
2018-06-20 11:15:20 -0400

    formatting changes

--- assets/javascript/work.js
+++ assets/javascript/work.js
@@ -1,5 +1,5 @@
 
-var games = ["Halo Inifinity", "The Last of Us Part II", "Death Stranding", "Call of Duty: Black Ops 4"];
+var games = ["Halo Infinity", "The Last of Us Part II", "Death Stranding", "Call of Duty: Black Ops 4"];
 
 function doWork () {
     $("button").on("click", function() {
@@ -16,15 +16,15 @@ function doWork () {
         var results = response.data
 
         for (var i = 0; i < results.length; i++) {
-            var imageDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
+            var imageDiv = $("<br><div class = 'row'><div class = 'col-lg-12'><div></div></div></div>");
+            var p = $("<span>").html("<h5>Rating: " + results[i].rating + "</h5>");
             var gameImage = $("<img>");
             gameImage.attr("src", results[i].images.fixed_width_still.url);
             gameImage.attr("data-state", "still");
             gameImage.attr("data-animate", results[i].images.fixed_width.url);
             gameImage.attr("data-still"  , results[i].images.fixed_width_still.url);
             gameImage.addClass("gif");
-            imageDiv.prepend(p);
+            imageDiv.append(p);
             imageDiv.prepend(gameImage);
             $("#imageViewer").prepend(imageDiv);
                 
@@ -58,7 +58,7 @@ function renderButtons () {
     $("#buttonViewer").empty();
     for (var i = 0; i < games.length; i++) {
         var button = $("<button>");
-        button.addClass("game");
+        button.addClass("game btn btn-outline-dark");
         button.attr("title", games[i]);
         button.text(games[i]);
         $("#buttonViewer").append(button);
